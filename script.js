63f7701a2eff3226279127cf1838b9796e906288
GBachheimer <gbachheimer@icloud.com>
2022-07-04 15:11:06 +0300

    draw snake, basic movement, basic margins

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 var xHeaderSnake = 0;
 var dxHeaderSnake = 4;
-var start = 0;
-//patrat = 40, 40*15 = 600 latura gameBoard
 
 function headerSnake() {
-    const canvas = document.getElementById("headerCanvas");
-    const c = canvas.getContext("2d");
+    const canv = document.getElementById("headerCanvas");
+    const cax = canv.getContext("2d");
     const image = new Image();
     image.src = "snake.png";
-    c.clearRect(0, 0, 400, 400);
+    cax.clearRect(0, 0, 400, 400);
     requestAnimationFrame(headerSnake);
-    c.drawImage(image, xHeaderSnake, 5, 120, 50);
+    cax.drawImage(image, xHeaderSnake, 5, 120, 50);
     if (xHeaderSnake > 270) {
         dxHeaderSnake = 16;
         dxHeaderSnake = -dxHeaderSnake;
@@ -21,47 +19,72 @@ function headerSnake() {
     xHeaderSnake += dxHeaderSnake;
 }
 
-function startGame() {
-    document.getElementById("message").innerHTML = "Press an arrow to start!";
-    document.getElementById("start").innerHTML = "RESET";
-    document.getElementById("start").onclick = function() { reset(); };
+var headPosition = [160, 280];
+var gridSize = 40;
+var direction = 1;
+let time;
+
+function drawSquare() {
     const canvas = document.getElementById("root");
-    const c = canvas.getContext("2d");
-    c.fillStyle = "green";
-    c.fillRect(120, 280, 80, 40);
-    generateFood(c);
-    start = 1;
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = '#35a255';
+    drawSnake(ctx);
+    time = setInterval(moveSnake, 100);
 }
 
-function reset() {
-    location.reload();
+document.onkeydown = function(event) {
+    var keyCode = window.event.keyCode;
+    switch (keyCode) {
+            case 37: //left
+                direction = 3;
+                break;
+            case 38: //up
+                direction = 2;
+                break;
+            case 39: //right
+                direction = 1;
+                break;
+            case 40: //down
+                direction = 4;
+                break;
+            default:
+                break;
+    }
+    moveSnake();
 }
 
-document.onkeydown = function(e) {
-    e = e || window.event;
-    var key = e.which || e.keyCode;
-    if ((key === 37 || key === 38 || key === 39 || key === 40) && start === 1) {
-        direction(key);
-        document.getElementById("message").innerHTML = "";
+function drawSnake(ctx) {
+    ctx.fillRect(headPosition[0], headPosition[1], gridSize, gridSize);
+}
+
+function moveSnake() {
+    const canvas = document.getElementById("root");
+    const ctx = canvas.getContext("2d");
+    switch (direction) {
+            case 3: //left
+                headPosition[0] = headPosition[0] - gridSize;
+                drawSnake(ctx);
+                break;
+            case 2: //up
+                headPosition[1] = headPosition[1] - gridSize;
+                drawSnake(ctx);
+                break;
+            case 1: //right
+                headPosition[0] = headPosition[0] + gridSize;
+                drawSnake(ctx);
+                break;
+            case 4: //down
+                headPosition[1] = headPosition[1] + gridSize;
+                drawSnake(ctx);
+                break;
+            default:
+                break;
+    }
+    checkWalls();
+}
+
+function checkWalls() {
+    if (headPosition[0] === -40 || headPosition[0] === 600 || headPosition[1] === -40 || headPosition[1] === 600) {
+        alert("Game Over!");
     }
 }
-
-function direction(key) {
-    alert(key + "was pressed");
-}
-
-function generateFood(c) {
-    var xFood = Math.floor(Math.random() * 15) * 40;
-    var yFood = Math.floor(Math.random() * 15) * 40;
-    c.fillStyle = "red";
-    c.fillRect(xFood, yFood, 40, 40);
-}
-
-/*function feedSnake() {
-    
-}
-
-function gameOver() {
-    
-}*/
-
